364ba25758e535f67866015341f536d407d19098
paokyereagy <p.okyereagy@alustudent.com>
2026-04-27 15:59:49 -0700

    push

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    # Iterate through keys in sorted order
+    for key in sorted(a_dictionary):
+        print(f"{key}: {a_dictionary[key]}")
