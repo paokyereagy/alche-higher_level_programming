3b9d8f0c03ca89d5100dddab2a8c6a2839657ab2
paokyereagy <p.okyereagy@alustudent.com>
2026-04-27 15:53:16 -0700

    push

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    # Return a new set containing elements present in both sets
-    return set_1 & set_2
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    count = 0
+    for key in a_dictionary:
+        count += 1
+    return count
