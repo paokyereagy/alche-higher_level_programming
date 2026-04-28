b0f5095a7dc0d1a1936cdbf2922d85b9bc1e0110
paokyereagy <p.okyereagy@alustudent.com>
2026-04-28 03:38:32 -0700

    push

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -12,7 +12,6 @@ class Square:
     def __init__(self, size):
         """
         Initialize a new Square instance.
-
         Args:
             size: The size of the square (no type/value verification).
         """
