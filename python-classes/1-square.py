dc5d5c888e92882036649e55088c4f71fba91153
paokyereagy <p.okyereagy@alustudent.com>
2026-04-28 03:33:05 -0700

    push

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
+
+class Square:
+    """
+    Class that defines a square by a private instance attribute: size.
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a new Square instance.
+
+        Args:
+            size: The size of the square (no type/value verification).
+        """
+        self.__size = size
