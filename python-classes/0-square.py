4f6146b102944b8955086ca9de1b704d14f7ea4f
paokyereagy <p.okyereagy@alustudent.com>
2026-04-28 03:29:37 -0700

    push

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This module defines an empty Square class.
+"""
+
 class Square:
-    """Defines an empty Square class"""
+    """
+    Empty class that defines a square.
+    """
     pass
